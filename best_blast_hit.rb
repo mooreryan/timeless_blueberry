f039cf9e85c10db530f104a953308ffa185ab10e
Ryan Moore <moorer@udel.edu>
2016-06-08 14:14:09 -0400

    Update

--- best_blast_hit.rb
+++ best_blast_hit.rb
@@ -3,8 +3,10 @@ Signal.trap("PIPE", "EXIT")
 
 blast = {}
 
+n = 0
 ARGV.each do |fname|
   File.open(fname).each_line do |line|
+    n+=1;STDERR.printf("Reading -- %d\r", n) if (n % 10_000).zero?
     ary = line.chomp.split "\t"
     query = ary[0]
     evalue = ary[10].to_f
@@ -15,4 +17,8 @@ ARGV.each do |fname|
       blast[query] = { line: line.chomp, evalue: evalue }
     end
   end
+end
+
+blast.each do |query, info|
+  puts info[:line]
 end
