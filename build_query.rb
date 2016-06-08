f9870fb37ef4797a146192844a30399fb62c3573
Ryan Moore <moorer@udel.edu>
2016-06-08 12:47:00 -0400

    Add script

---
+++ build_query.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+Signal.trap("PIPE", "EXIT")
+
+ids = []
+
+File.open(ARGV.first).each_line do |line|
+  ids << line.chomp
+end
+
+puts "select ncbi_taxon from uniref50_to_taxonomy where uniref50 in ("
+
+ids.each_with_index do |id, idx|
+  if idx.zero?
+    puts "'#{id}'"
+  else
+    puts ",'#{id}'"
+  end
+end
+
+puts ";"
