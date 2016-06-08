4cadbab232abc981fa6d377d4b7f43930ffebeaa
Ryan Moore <moorer@udel.edu>
2016-06-08 14:20:40 -0400

    Update

--- build_query.rb
+++ build_query.rb
@@ -7,7 +7,7 @@ File.open(ARGV.first).each_line do |line|
   ids << line.chomp
 end
 
-puts "select ncbi_taxon from uniref50_to_taxonomy where uniref50 in ("
+puts "select * from uniref50_to_taxonomy where uniref50 in ("
 
 ids.each_with_index do |id, idx|
   if idx.zero?
