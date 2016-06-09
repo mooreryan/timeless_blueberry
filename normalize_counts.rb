238ada15fc8bc8bf708fe2f5dd03c574aad9d0b8
Ryan Moore <moorer@udel.edu>
2016-06-08 21:05:16 -0400

    Update

---
+++ normalize_counts.rb
@@ -0,0 +1,16 @@
+ARGV.each do |fname|
+  File.open("#{fname}.normalized_counts", "w") do |f|
+    tax_counts = {}
+    total = 0
+    File.open(fname).each_line do |line|
+      count, tax = line.chomp.split
+
+      total += count.to_i
+      tax_counts[tax] = count.to_i
+    end
+
+    tax_counts.each do |tax, count|
+      puts [count / total.to_f, count, tax].join "\t"
+    end
+  end
+end
